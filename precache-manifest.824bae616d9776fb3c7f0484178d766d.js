40197b627e1c31a326fcad71caaf53341d4a9475
harih4ran <hariharank.offl@gmail.com>
2023-01-01 15:25:55 +0530

    Updates

---
+++ precache-manifest.824bae616d9776fb3c7f0484178d766d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ea563e4d1309563d4246",
+    "url": "/static/css/main.baa424b1.chunk.css"
+  },
+  {
+    "revision": "ea563e4d1309563d4246",
+    "url": "/static/js/main.ea563e4d.chunk.js"
+  },
+  {
+    "revision": "bb2d15f8b100106d30b0",
+    "url": "/static/js/1.bb2d15f8.chunk.js"
+  },
+  {
+    "revision": "e1a86fe6686ab4034ef0",
+    "url": "/static/js/2.e1a86fe6.chunk.js"
+  },
+  {
+    "revision": "2ff3dd98650ffb28a13e",
+    "url": "/static/js/runtime~main.2ff3dd98.js"
+  },
+  {
+    "revision": "0846fc8f063169c65f97dfafb5d4ba4e",
+    "url": "/index.html"
+  }
+];
